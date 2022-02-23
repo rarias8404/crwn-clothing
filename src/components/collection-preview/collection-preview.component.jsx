3f8ffe1cf9767b799bf4073964c36b53d0b32d77
rarias8404 <renier.arias@gmail.com>
2022-02-23 18:37:50 -0500

    created respective selectors and updated corresponding components with new redux flow

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,7 +3,7 @@ import CollectionItem from "../collection-item/collection-item.component";
 
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({ title, items}) => {
+const CollectionPreview = ({title, items}) => {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items}) => {
         {items
           .filter((item, idx) => idx < 4)
           .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
+            <CollectionItem key={item.id} item={item}/>
+          ))}
       </div>
     </div>
   );
