b3333f7a52cf674fa75fce5c50d87d6b0570d12d
rarias8404 <renier.arias@gmail.com>
2022-02-23 16:29:39 -0500

    move directory data and shop data to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,29 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections} from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
-import SECTIONS_DATA from './sections.data'
 
 import './directory.styles.scss'
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({id, ...otherSectionProps}) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: SECTIONS_DATA
-    }
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
-  render() {
-    const {sections} = this.state
-    return (
-      <div className="directory-menu">
-        {sections.map(({id, ...otherSectionProps}) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
