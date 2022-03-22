e9515ad7906e2312bfffbdfbd7f15c2abb1849c6
Renier Arias Granda <renier.arias@gmail.com>
2022-03-22 15:55:07 -0400

    [wip] converting components over to styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,29 +4,31 @@ import { withRouter } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
-
-import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
-import './cart-dropdown.styles.scss'
+import { CartDropDownContainer, CartItemsContainer, EmptyMessage, CartDropdownButton } from "./cart-dropdown.styles";
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className='cart-dropdown'>
-    <div className="cart-items">
-      {
-        cartItems.length ? (
-        cartItems.map(cartItem =>
-        <CartItem key={cartItem.id} item={cartItem} />
-      )) : (
-        <span className='empty-message'>Your cart is empty</span>
-      )}
-    </div>
-    <CustomButton onClick={() => {
-      history.push('/checkout')
-      dispatch(toggleCartHidden())
-    }}>GO TO CHECKOUT</CustomButton>
-  </div>
-)
+const CartDropdown = ({ cartItems, history, dispatch }) => {
+  const handleClick = () => {
+    history.push('/checkout')
+    dispatch(toggleCartHidden())
+  }
+
+  return (
+    <CartDropDownContainer>
+      <CartItemsContainer>
+        {
+          cartItems.length ? (
+            cartItems.map(cartItem =>
+              <CartItem key={cartItem.id} item={cartItem} />
+            )) : (
+            <EmptyMessage>Your cart is empty</EmptyMessage>
+          )}
+      </CartItemsContainer>
+      <CartDropdownButton onClick={handleClick}>GO TO CHECKOUT</CartDropdownButton>
+    </CartDropDownContainer>
+  )
+}
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
